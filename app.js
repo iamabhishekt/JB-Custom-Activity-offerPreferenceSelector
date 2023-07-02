62d6a06d2475ba825f7f385fd916684381f02c91
iamabhishekt <abhishekthakur.art@protonmail.com>
2023-07-02 19:49:32 -0400

    app.js changes

--- app.js
+++ app.js
@@ -1,7 +1,50 @@
-var express = require( 'express' )
-var app = express()
-var port = process.env.PORT || 3000
+// app.js
 
-app.use( express.static('public') )
+var express = require( 'express' );
+var bodyParser = require('body-parser');
+var app = express();
+var port = process.env.PORT || 3000;
 
-app.listen( port, () => console.log( `App listening on port ${port}!`) )
+// support json encoded bodies
+app.use(bodyParser.json()); 
+
+app.use( express.static('public') );
+
+app.post( '/execute', (req, res) => {
+    // Execute logic here
+    console.log('Execute route called');
+    console.log(req.body);
+    // You'll probably want to do something with the incoming execute request
+    // For now we're just going to respond with a 200
+    res.status(200).end();
+});
+
+app.post( '/save', (req, res) => {
+    // Save logic here
+    console.log('Save route called');
+    console.log(req.body);
+    res.status(200).end();
+});
+
+app.post( '/publish', (req, res) => {
+    // Publish logic here
+    console.log('Publish route called');
+    console.log(req.body);
+    res.status(200).end();
+});
+
+app.post( '/validate', (req, res) => {
+    // Validation logic here
+    console.log('Validate route called');
+    console.log(req.body);
+    res.status(200).end();
+});
+
+app.post( '/stop', (req, res) => {
+    // Stop logic here
+    console.log('Stop route called');
+    console.log(req.body);
+    res.status(200).end();
+});
+
+app.listen( port, () => console.log( `App listening on port ${port}!`) );
