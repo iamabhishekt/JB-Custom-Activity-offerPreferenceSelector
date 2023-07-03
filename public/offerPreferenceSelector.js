05d90e00a3e2169898e0a756f56fbcedb0160f18
iamabhishekt <abhishekthakur.art@protonmail.com>
2023-07-02 21:38:59 -0400

    added jquery and require

--- public/offerPreferenceSelector.js
+++ public/offerPreferenceSelector.js
@@ -1,143 +1,203 @@
 define(["postmonger"], function (Postmonger) {
-  "use strict";
-
-  var connection = new Postmonger.Session();
-  var payload = {};
-  var steps = [
-    { label: "Select Offer Preference", key: "step1" },
-    { label: "Summary", key: "step2" }
-  ];
-  var currentStep = steps[0].key;
-
-  $(window).ready(onRender);
-
-  connection.on("initActivity", initialize);
-  connection.on("requestedTokens", onGetTokens);
-  connection.on("requestedEndpoints", onGetEndpoints);
-
-  connection.on("clickedNext", onClickedNext);
-  connection.on("clickedBack", onClickedBack);
-  connection.on("gotoStep", onGotoStep);
-
-  function onRender() {
-    connection.trigger("ready");
-    connection.trigger("requestTokens");
-    connection.trigger("requestEndpoints");
-
-    $("#offerPreference").change(function () {
-      var message = getOfferPreference();
-      connection.trigger("updateButton", {
-        button: "next",
-        enabled: Boolean(message),
+    "use strict";
+  
+    var connection = new Postmonger.Session();
+    var payload = {};
+    var lastStepEnabled = false;
+    var steps = [
+      // initialize to the same value as what's set in config.json for consistency
+      { label: "Step 1", key: "step1" },
+      { label: "Step 2", key: "step2" },
+      { label: "Step 3", key: "step3" },
+      { label: "Step 4", key: "step4", active: false },
+    ];
+    var currentStep = steps[0].key;
+  
+    $(window).ready(onRender);
+  
+    connection.on("initActivity", initialize);
+    connection.on("requestedTokens", onGetTokens);
+    connection.on("requestedEndpoints", onGetEndpoints);
+  
+    connection.on("clickedNext", onClickedNext);
+    connection.on("clickedBack", onClickedBack);
+    connection.on("gotoStep", onGotoStep);
+  
+    function onRender() {
+      // JB will respond the first time 'ready' is called with 'initActivity'
+      connection.trigger("ready");
+  
+      connection.trigger("requestTokens");
+      connection.trigger("requestEndpoints");
+  
+      // Disable the next button if a value isn't selected
+      $("#offerPreference").change(function () {
+        var offerPreference = getOfferPreference();
+        connection.trigger("updateButton", {
+          button: "next",
+          enabled: Boolean(offerPreference),
+        });
+  
+        $("#message").html(offerPreference);
       });
-    });
-  }
-
-  function initialize(data) {
-    if (data) {
-      payload = data;
+  
+      // Toggle step 4 active/inactive
+      // If inactive, wizard hides it and skips over it during navigation
+      $("#toggleLastStep").click(function () {
+        lastStepEnabled = !lastStepEnabled; // toggle status
+        steps[3].active = !steps[3].active; // toggle active
+  
+        connection.trigger("updateSteps", steps);
+      });
     }
-
-    var offerPreference;
-    var hasInArguments = Boolean(
+  
+    function initialize(data) {
+      if (data) {
+        payload = data;
+      }
+  
+      var offerPreference;
+      var hasInArguments = Boolean(
         payload["arguments"] &&
-        payload["arguments"].execute &&
-        payload["arguments"].execute.inArguments &&
-        payload["arguments"].execute.inArguments.length > 0
-    );
-
-    var inArguments = hasInArguments
+          payload["arguments"].execute &&
+          payload["arguments"].execute.inArguments &&
+          payload["arguments"].execute.inArguments.length > 0
+      );
+  
+      var inArguments = hasInArguments
         ? payload["arguments"].execute.inArguments
         : {};
-
-    $.each(inArguments, function (index, inArgument) {
-      $.each(inArgument, function (key, val) {
-        if (key === "offerPreference") {
-          offerPreference = val;
-        }
+  
+      $.each(inArguments, function (index, inArgument) {
+        $.each(inArgument, function (key, val) {
+          if (key === "offerPreference") {
+            offerPreference = val;
+          }
+        });
       });
-    });
-
-    if (!offerPreference) {
-      showStep(null, 1);
-      connection.trigger("updateButton", { button: "next", enabled: false });
-    } else {
-      $("#offerPreference")
-        .find("option[value=" + offerPreference + "]")
-        .attr("selected", "selected");
-      showStep(null, 2);
+  
+      // If there is no offerPreference selected, disable the next button
+      if (!offerPreference) {
+        showStep(null, 1);
+        connection.trigger("updateButton", { button: "next", enabled: false });
+        // If there is a offerPreference, skip to the summary step
+      } else {
+        $("#offerPreference")
+          .find("option[value=" + offerPreference + "]")
+          .attr("selected", "selected");
+        $("#message").html(offerPreference);
+        showStep(null, 3);
+      }
     }
-  }
-
-  function onGetTokens(tokens) {
-    console.log(tokens);
-  }
-
-  function onGetEndpoints(endpoints) {
-    console.log(endpoints);
-  }
-
-  function onClickedNext() {
-    if (currentStep.key === "step2") {
-      save();
-    } else {
-      connection.trigger("nextStep");
+  
+    function onGetTokens(tokens) {
+      // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
+      // console.log(tokens);
     }
-  }
-
-  function onClickedBack() {
-    connection.trigger("prevStep");
-  }
-
-  function onGotoStep(step) {
-    showStep(step);
-    connection.trigger("ready");
-  }
-
-  function showStep(step, stepIndex) {
-    if (stepIndex && !step) {
-      step = steps[stepIndex - 1];
+  
+    function onGetEndpoints(endpoints) {
+      // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
+      // console.log(endpoints);
     }
-
-    currentStep = step;
-
-    $(".step").hide();
-
-    switch (currentStep.key) {
-      case "step1":
-        $("#step1").show();
-        connection.trigger("updateButton", {
-          button: "next",
-          enabled: Boolean(getOfferPreference()),
-        });
-        connection.trigger("updateButton", {
-          button: "back",
-          visible: false,
-        });
-        break;
-      case "step2":
-        $("#step2").show();
-        connection.trigger("updateButton", {
-          button: "back",
-          visible: true,
-        });
-        connection.trigger("updateButton", {
-          button: "next",
-          text: "done",
-          visible: true,
-        });
-        break;
+  
+    function onClickedNext() {
+      if (
+        (currentStep.key === "step3" && steps[3].active === false) ||
+        currentStep.key === "step4"
+      ) {
+        save();
+      } else {
+        connection.trigger("nextStep");
+      }
     }
-  }
-
-  function save() {
-    var offerPreference = getOfferPreference();
-    payload["arguments"].execute.inArguments = [{ offerPreference: offerPreference }];
-    payload["metaData"].isConfigured = true;
-    connection.trigger("updateActivity", payload);
-  }
-
-  function getOfferPreference() {
-    return $("#offerPreference").find("option:selected").val().trim();
-  }
-});
+  
+    function onClickedBack() {
+      connection.trigger("prevStep");
+    }
+  
+    function onGotoStep(step) {
+      showStep(step);
+      connection.trigger("ready");
+    }
+  
+    function showStep(step, stepIndex) {
+      if (stepIndex && !step) {
+        step = steps[stepIndex - 1];
+      }
+  
+      currentStep = step;
+  
+      $(".step").hide();
+  
+      switch (currentStep.key) {
+        case "step1":
+          $("#step1").show();
+          connection.trigger("updateButton", {
+            button: "next",
+            enabled: Boolean(getOfferPreference()),
+          });
+          connection.trigger("updateButton", {
+            button: "back",
+            visible: false,
+          });
+          break;
+        case "step2":
+          $("#step2").show();
+          connection.trigger("updateButton", {
+            button: "back",
+            visible: true,
+          });
+          connection.trigger("updateButton", {
+            button: "next",
+            text: "next",
+            visible: true,
+          });
+          break;
+        case "step3":
+          $("#step3").show();
+          connection.trigger("updateButton", {
+            button: "back",
+            visible: true,
+          });
+          if (lastStepEnabled) {
+            connection.trigger("updateButton", {
+              button: "next",
+              text: "next",
+              visible: true,
+            });
+          } else {
+            connection.trigger("updateButton", {
+              button: "next",
+              text: "done",
+              visible: true,
+            });
+          }
+          break;
+        case "step4":
+          $("#step4").show();
+          break;
+      }
+    }
+  
+    function save() {
+      var name = $("#offerPreference").find("option:selected").html();
+      var value = getOfferPreference();
+  
+      // 'payload' is initialized on 'initActivity' above.
+      // Journey Builder sends an initial payload with defaults
+      // set by this activity's config.json file.  Any property
+      // may be overridden as desired.
+      payload.name = name;
+  
+      payload["arguments"].execute.inArguments = [{ offerPreference: value }];
+  
+      payload["metaData"].isConfigured = true;
+  
+      connection.trigger("updateActivity", payload);
+    }
+  
+    function getOfferPreference() {
+      return $("#offerPreference").find("option:selected").attr("value").trim();
+    }
+  });
+  
